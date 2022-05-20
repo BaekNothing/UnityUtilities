0729ab0b1c6647b712c603d5ded67a9f8e6050bf
BaekNothing <47516849+BaekNothing@users.noreply.github.com>
2022-05-20 19:04:31 +0900

    Update OptimizedScrollview.cs
    - Fixed an issue where position errors accumulated when selected short enough to prevent movement.  
    - Fixed an issue where the formula was incorrect when checking the top hard cap  
    - Cleaning up duplicate code

--- UI/OptimizedScrollview.cs
+++ UI/OptimizedScrollview.cs
@@ -6,26 +6,29 @@ using UnityEngine.EventSystems;
 
 public class OptimizedScrollview : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    [SerializeField] protected int index = 0;
+    protected int index = 0;
     public int getIndex() => index;
     public void setIndex(int inputIndex)
     {
         index = inputIndex;
         if (index > cellCount - cellCountInContent)
             index = cellCount - cellCountInContent;
+        if (index < 0)
+            index = 0;
+
+        PositionRearrangement();
     }
 
     protected int cellCount = 0;
     protected int cellCountInViewport = 0;
     protected int cellCountInContent = 0;
-    
-    float cellSize;
+    protected float cellSize;
 
     public bool isReverse = false;
     public RectTransform thisRect;
     public RectTransform viewport;
     public RectTransform content;
-    float recoverySpeed = 15f;
+    float recoverySpeed = 22f;
     float cap_Hard_ContentPosValue = 100f;
     float cap_contentPosUP = 0f;
     Vector2 scrollDir = new Vector2(0, 1);
@@ -34,6 +37,7 @@ public class OptimizedScrollview : MonoBehaviour, IPointerDownHandler, IPointerU
     Vector2? originPos;
     Vector2 nowPos = new Vector2(0, 0);
     Vector2 originContentPos;
+    Vector2 posDiff = new Vector2(0, 0);
 
     //private void Start() => init();
     public void Init(int inputCellCount)
@@ -47,13 +51,12 @@ public class OptimizedScrollview : MonoBehaviour, IPointerDownHandler, IPointerU
 
         cellCount = inputCellCount;
         cellSize = content.GetChild(0).GetComponent<RectTransform>().sizeDelta.y;
-        cap_Hard_ContentPosValue = cellSize + cellSize * 0.5f;
+        cap_Hard_ContentPosValue = Mathf.Min(cellSize, 120f);
         cellCountInViewport = 1;
         while (cellSize * cellCountInViewport < thisRect.sizeDelta.y)
             cellCountInViewport++;
         cellCountInContent = content.transform.childCount;
         scrollDir = new Vector2(0, 1);
-        SetCell();
     }
 
     protected void SetCell()
@@ -65,14 +68,37 @@ public class OptimizedScrollview : MonoBehaviour, IPointerDownHandler, IPointerU
 
     public void OnPointerDown(PointerEventData data)
     {
+        //PositionRearrangement();
         originPos = data.position;
         originContentPos = content.anchoredPosition;
     }
 
-    public void OnPointerUp(PointerEventData data) => originPos = null;
+    public void OnPointerUp(PointerEventData data)
+    {
+        originPos = null;
+        PositionRearrangement();
+    }
+
+    protected void PositionRearrangement()
+    {
+        float topSpace = content.anchoredPosition.y;
+        float bottomSpace = content.sizeDelta.y - (content.anchoredPosition.y + thisRect.sizeDelta.y);
+
+        if (index < cellCount - cellCountInContent && bottomSpace < cellSize / 2)
+        {
+            index += 1;
+            content.anchoredPosition -= cellSize * scrollDir;
+            SetCell();
+        }
+        else if (0 < index && topSpace < cellSize / 2)
+        {
+            index += -1;
+            content.anchoredPosition += cellSize * scrollDir;
+            SetCell();
+        }
+    }
 
     public void Refresh() => SetCell();
-    
    
     private void LateUpdate()
     {
@@ -80,69 +106,112 @@ public class OptimizedScrollview : MonoBehaviour, IPointerDownHandler, IPointerU
             return;
         if (originPos != null)
         {
-            if (!(Input.GetMouseButton(0) || Input.touchCount > 0))
-            {
-                originPos = null;
+            if (!GetInputSetPos())
                 return;
-            }
-            if (Input.GetMouseButton(0)) nowPos = Input.mousePosition;
-            if (Input.touchCount > 0) nowPos = Input.GetTouch(0).position;
 
-            if (cap_contentPosUP <= 0f)
-                cap_contentPosUP = content.sizeDelta.y - thisRect.sizeDelta.y;
-            Vector2 posDiff = (nowPos - (Vector2)originPos) * scrollDir;
+            posDiff = (nowPos - (Vector2)originPos) * scrollDir;
             if (isReverse)
                 posDiff *= -1;
 
-            if (posDiff.magnitude > cellSize && !isReverse)
+            if (posDiff.magnitude > cellSize)
             {
-                if (((Vector2)originPos).y > nowPos.y && index > 0)
+                if (CheckNextToPrev(isReverse))
                 {
                     index--;
-                    originPos = nowPos;
-                    posDiff = new Vector2(0, 0);
-                    SetCell();
+                    SetMoveFlagAndPos();
                 }
-                if (((Vector2)originPos).y < nowPos.y && index < cellCount - cellCountInContent)
+                if (CheckPrevToNext(isReverse))
                 {
                     index++;
-                    originPos = nowPos;
-                    posDiff = new Vector2(0, 0);
-                    SetCell();
+                    SetMoveFlagAndPos();
                 }
             }
-            if (posDiff.magnitude > cellSize && isReverse)
-            {
-                if (((Vector2)originPos).y < nowPos.y && index > 0)
-                {
-                    index--;
-                    originPos = nowPos;
-                    posDiff = new Vector2(0, 0);
-                    SetCell();
-                }
-                if (((Vector2)originPos).y > nowPos.y && index < cellCount - cellCountInContent)
-                {
-                    index++;
-                    originPos = nowPos;
-                    posDiff = new Vector2(0, 0);
-                    SetCell();
-                }
-            }
-
             Vector2 destPos = originContentPos + posDiff;
             content.anchoredPosition = destPos;
 
-            if (content.anchoredPosition.y < -cap_Hard_ContentPosValue)
-                content.anchoredPosition = new Vector2(0, -cap_Hard_ContentPosValue);
-            if(content.anchoredPosition.y > cap_Hard_ContentPosValue + cap_contentPosUP)
-                content.anchoredPosition = new Vector2(0, cap_Hard_ContentPosValue + cap_contentPosUP);
+            BlockPosHardCap();
         }
         else
+            RecoveryPos();
+    }
+
+
+    protected bool GetInputSetPos()
+    {
+        if (!(Input.GetMouseButton(0) || Input.touchCount > 0))
         {
-            if (content.anchoredPosition.y < 0)
-                content.anchoredPosition += scrollDir * recoverySpeed;
-            if (content.anchoredPosition.y > cap_contentPosUP)
-                content.anchoredPosition -= scrollDir * recoverySpeed;
+            originPos = null;
+            return false;
         }
+        if (Input.GetMouseButton(0)) nowPos = Input.mousePosition;
+        if (Input.touchCount > 0) nowPos = Input.GetTouch(0).position;
+
+        return true;
+    }
+
+    protected void BlockPosHardCap()
+    {
+        if (content.sizeDelta.y < thisRect.sizeDelta.y)
+            content.anchoredPosition = new Vector2(0, 0);
+        if (cap_contentPosUP <= 0f && (cap_contentPosUP = content.sizeDelta.y - thisRect.sizeDelta.y) <= 0f)
+            return;
+
+        if (content.anchoredPosition.y < -cap_Hard_ContentPosValue)
+            content.anchoredPosition = new Vector2(0, -cap_Hard_ContentPosValue);
+        if (content.anchoredPosition.y > cap_Hard_ContentPosValue + cap_contentPosUP)
+            content.anchoredPosition = new Vector2(0, cap_Hard_ContentPosValue + cap_contentPosUP);
+
+        // It Blocked But Not End of Index;
+        if (index > 0 && content.anchoredPosition.y <= -cap_Hard_ContentPosValue)
+        {
+            index--;
+            content.anchoredPosition += cellSize * scrollDir;
+            SetCell();
+        }
+        if (index < cellCount - cellCountInContent && content.anchoredPosition.y >= cap_Hard_ContentPosValue + cap_contentPosUP)
+        {
+            index++;
+            content.anchoredPosition -= cellSize * scrollDir;
+            SetCell();
+        }
+        SetCell();
+    }
+
+    protected void RecoveryPos()
+    {
+        if (cap_contentPosUP <= 0f)
+            cap_contentPosUP = content.sizeDelta.y - thisRect.sizeDelta.y;
+        if (cap_contentPosUP <= 0f)
+            return;
+
+        if (content.anchoredPosition.y < 0)
+            content.anchoredPosition += scrollDir * recoverySpeed;
+        if (content.anchoredPosition.y > cap_contentPosUP)
+            content.anchoredPosition -= scrollDir * recoverySpeed;
+    }
+
+    protected bool CheckNextToPrev(bool isReverse)
+    {
+        if (isReverse && ((Vector2)originPos).y < nowPos.y && index > 0)
+            return true;
+        if (!isReverse && ((Vector2)originPos).y > nowPos.y && index > 0)
+            return true;
+        return false;
+    }
+
+    protected bool CheckPrevToNext(bool isReverse)
+    {
+        if (isReverse && ((Vector2)originPos).y > nowPos.y && index < cellCount - cellCountInContent)
+            return true;
+        if (!isReverse && ((Vector2)originPos).y < nowPos.y && index < cellCount - cellCountInContent)
+            return true;
+        return false;
+    }
+
+    protected void SetMoveFlagAndPos()
+    {
+        originPos = nowPos;
+        posDiff = new Vector2(0, 0);
+        SetCell();
     }
 }
